3bb3b478bae51b06f9c9c7a313c947c1b0ee6643
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-06-15 16:45:51 -0500

    add loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,14 @@ searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
   searchForm.classList.toggle('active');
 }
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut(){
+    setTimeout(loader, 3000);
+}
+
+
+  
