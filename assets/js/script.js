3232c6e1a18cd8df0f8cd74d66a12eea7b60e0b9
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-06-22 01:17:56 -0500

    refactor js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,59 +14,27 @@ document.querySelector("#close-login-btn").onclick = () => {
   loginForm.classList.remove("active");
 };
 
-function setFormMessage(formElement, type, message) {
-  const messageElement = formElement.querySelector(".form-message");
-
-  messageElement.textContent = message;
-  messageElement.classList.remove("form-message--success", "form-message--error");
-  messageElement.classList.add(`form-message--${type}`);
-}
-
-function setInputError(inputElement, message) {
-  inputElement.classList.add("form-input--error");
-  inputElement.parentElement.querySelector(".form-input-error-message").textContent = message;
-}
-
-function clearInputError(inputElement) {
-  inputElement.classList.remove("form-input--error");
-  inputElement.parentElement.querySelector(".form-input-error-message").textContent = "";
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#createAccount");
 
   document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-      e.preventDefault();
-      loginForm.classList.add("form--hidden");
-      createAccountForm.classList.remove("form--hidden");
+    e.preventDefault();
+    loginForm.classList.add("form--hidden");
+    createAccountForm.classList.remove("form--hidden");
   });
 
   document.querySelector("#linkLogin").addEventListener("click", e => {
-      e.preventDefault();
-      loginForm.classList.remove("form--hidden");
-      createAccountForm.classList.add("form--hidden");
+    e.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    createAccountForm.classList.add("form--hidden");
   });
 
   loginForm.addEventListener("submit", e => {
-      e.preventDefault();
+    e.preventDefault();
 
-      // Perform your AJAX/Fetch login
-
-      setFormMessage(loginForm, "error", "Invalid username/password combination");
+    // Perform your AJAX/Fetch login
   });
-
-  // document.querySelectorAll(".form-input").forEach(inputElement => {
-  //     inputElement.addEventListener("blur", e => {
-  //         if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 7) {
-  //             setInputError(inputElement, "Username must be at least 7 characters in length");
-  //         }
-  //     });
-
-      // inputElement.addEventListener("input", e => {
-      //     clearInputError(inputElement);
-      // });
-  // });
 });
 
 // Signup function
