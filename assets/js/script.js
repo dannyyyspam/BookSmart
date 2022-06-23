48ba95802848cbacdb04c322a4e62e6a91573a81
hayden <haydenburger43@gmail.com>
2022-06-22 19:29:10 -0500

    added randomPic

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,3 +93,19 @@ var getGoogleBooks = function(searchTerm) {
 
 }
 getGoogleBooks();
+
+
+var form = document.getElementById('form')
+
+form.addEventListner( 'submit',function(event){
+  Event.preventDefault()
+
+  var width = document.getElementById('width').value
+  var height = document.getElementById('height').value
+
+  var url = 'https://picsum.photos/${width}/${height}'
+
+  var image = document.getElementById('img')
+
+  image.src = url
+})
